6c46c8f8177d3977d704d5ac0eb76699537c82d8
Администратор <r83939@mail.ru>
2023-02-04 11:30:08 +0300

    Спринт 11 v3

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDaoImpl.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.dao.impl;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.dao.FilmDao;
+
+@Component
+public class FilmDaoImpl implements FilmDao {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/UserDaoImpl.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.dao.impl;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.dao.UserDao;
+
+@Component
+public class UserDaoImpl implements UserDao {
+}
